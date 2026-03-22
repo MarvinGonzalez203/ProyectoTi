e5071b34e281397ee3cdafcf5fe3ee7c52215604
Marvin David Ramirez <mramirezg49@miumg.edu.gt>
2026-03-22 17:20:20 -0600

    Migracion de usuarios
    creacion de migracion de usuarios

--- EjemploAdmonTi/Models/AppDbContext.cs
+++ EjemploAdmonTi/Models/AppDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace EjemploAdmonTi.Models
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjemploAdmonTi.Models
 {
-    public class AppDbContext
+    public class AppDbContext :
+        IdentityDbContext<IdentityUser>
     {
+
+        public AppDbContext
+          (DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
     }
 }
+
--- EjemploAdmonTi/Program.cs
+++ EjemploAdmonTi/Program.cs
@@ -1,8 +1,27 @@
+using EjemploAdmonTi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//desde aqui
+var connectionString = 
+    builder.Configuration.GetConnectionString
+    ("DefaultConnection");
+
+builder.Services.AddDbContext <AppDbContext>(
+    options => options.UseSqlServer
+    (connectionString), ServiceLifetime.Transient );
+
+builder.Services.AddDefaultIdentity<IdentityUser>
+    (options =>
+    options.SignIn.RequireConfirmedAccount = 
+    false).AddEntityFrameworkStores<AppDbContext>();
+
+//Hasta aqui
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
